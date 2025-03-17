5cd2f589c5b4a752d2c73ffe972f6e1d61fc22be
mucherla-chaitanya-kumar <mucherlachaitanya@gmail.com>
2025-03-17 15:05:47 +0530

    first commit

---
+++ area.java
@@ -0,0 +1,11 @@
+import Scanner.util.java
+public class area{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		int r=sc.nextInt();
+		float area=3.14*r*r;
+		System.out.println(area);
+	}
+}
+
