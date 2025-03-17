acb897865374b681ac1101ad556ce26ebefe5210
Chaitanya <73526408+mucherla-chaitanya-kumar@users.noreply.github.com>
2025-03-17 15:22:43 +0530

    Update area.java

--- area.java
+++ area.java
@@ -3,8 +3,8 @@ public class area{
 	public static void main(String[] args)
 	{
 		Scanner sc = new Scanner(System.in);
-		int r=sc.nextInt();
-		float area=3.14*r*r;
+		float r=sc.nextInt();
+		float area=(3.14*r*r)f;
 		System.out.println(area);
 	}
 }
